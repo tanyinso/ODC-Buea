10648c5add02af43616930ed621dffb8e013d071
tanyinso <tanyinsobright237@gmail.com>
2023-07-11 06:32:47 -0400

    initial commit

---
+++ Introduction.js
@@ -0,0 +1,60 @@
+// This is a comment
+//multiple line comment
+/*
+// this is a multi-line comment
+// Here is the second comment
+// */
+// // Variables declarations
+// //var, let, const
+// //var and let declare variables that can be changed at any time , while const declared variables can't be declared twice.
+// var myVariable=10;
+// let myVariableName="hello";
+// const variable=12
+
+// console.log(myVariable)
+// console.log(myVariableName)
+// console.log(variable)
+// myVariableName="Good Morning";
+// //variable=15
+// //console.log(variable)
+// console.log(myVariableName)
+// //This displays an error because const assigns a variable just once.
+// //There are 7 types of data types in java scripts.
+// console.log(typeof(variable))
+// console.log(typeof(myVariableName))
+// console.log(typeof(myVariable))
+// //undefined scenario
+// var initialVariable;
+// console.log(typeof(initialVariable))
+// //null definition
+// var initialVariable= null;
+// console.log(typeof(initialVariable))
+
+/** Operators in javascripts  */
+
+// Addition , subtraction, multiplication, division and modulo
+
+// var a=2, b=10;
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log(a % b);
+
+/********************************  increment and Decrement*****************************************/
+var increment = 3, decrement = 10;
+let temp=0
+let temp2=0
+console.log(increment, decrement);
+
+// temp = ++ increment + 2
+temp2=2 + increment ++
+console.log(temp)
+console.log(temp2)
+//increment++;
+//decrement = decrement - 1;
+//decrement--;
+//decrement-=1;
+//console.log(increment, decrement);
+
+//
